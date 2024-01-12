d39d887b4d712d32afe8775138709f6d09d7be3d
vimal <selvamvimaldz1@gmail.com>
2024-01-12 20:05:27 +0530

    modify getsalelist

--- src/controller/Sales/Sales.js
+++ src/controller/Sales/Sales.js
@@ -56,36 +56,30 @@ const getSalesData = async (req, res) => {
 
     try {
         const resData = await salesModel.find({ email: req.params.email }, { _id: 0, saleYear: 0 });
-        const productChart = resData.map((e) => {
-            return {
-                productName: e.productName,
-                quantity: e.quantity
-            }
-        })
+        console.log(resData);
+        // const productChart = resData.reduce((acc,e) => {
+        //     acc[e.saleMon]=acc[e.saleMon]??[];
+        //     acc[e.saleMon].push({
+        //         productName: e.productName,
+        //         quantity: e.quantity,
+        //         saleMon:e.saleMon})
+        //     return acc
+        // },{})
         const categoryData = resData.reduce((acc, e) => {
-            acc[e.category] = acc[e.category] || []
+            acc[e.category] = acc[e.category] ?? []
             acc[e.category].push({
                 productName: e.productName,
                 quantity: e.quantity
             })
-            //  console.log(acc);
             return acc
         }, {})
-        // console.log(categoryData);
         const category = resData.map((e) => e.category);
-        // console.log(category);
         const removeDup = category.filter((e, i) => category.indexOf(e) == i)
-        // console.log(removeDup);
-
         const cateListAmt = removeDup.map((e) => {
             return categoryData[e].map((c) => Number(c.quantity))
                 .reduce((prev, cur) => { return prev + parseInt(cur) }, 0)
         })
-        // const obj=
         let categotyList = []
-
-
-        // for(var y = 0; y < removeDup.length; y++){
         for (var i = 0; i < removeDup.length; i++) {
             let thing = {};
             thing['category'] = category[i];
@@ -93,14 +87,10 @@ const getSalesData = async (req, res) => {
             categotyList.push(thing)
 
         }
-        // console.log(resData);
 
         const getmon = resData.map((e) => e.saleMon);
-        // console.log(category);
         const removeMonDup = getmon.filter((e, i) => getmon.indexOf(e) == i)
-        // console.log(removeMonDup);
         const getMonData = resData.reduce((acc, e) => {
-            // console.log(e.saleMon);
             acc[e.saleMon] = acc[e.saleMon] || []
             acc[e.saleMon].push({
                 quantity: e.quantity,
@@ -108,24 +98,7 @@ const getSalesData = async (req, res) => {
             }
             )
             return acc
-            // .reduce((pre,e)=>{
-            //     pre[e.category]=pre[e.category]||[]
-            //     pre[e.category].push({
-            //         quantity:e.quantity,
-            //     category:e.category
-            //     })
-            // },[])
-
         }, [])
-        // removeDup
-        // const yeardata=getMon.map((e)=>{
-
-
-        // })
-        // const cateListAmt = removeDup.map((e) => {
-        //     return categoryData[e].map((c) => Number(c.quantity))
-        //         .reduce((prev, cur) => { return parseInt(prev) + parseInt(cur) })
-        // })
         const Mondata = removeMonDup.map((a) => {
             return getMonData[a].map((c) => Number(c.quantity))
                 .reduce((prev, cur) => { return prev + parseInt(cur) }, 0)
@@ -138,13 +111,8 @@ const getSalesData = async (req, res) => {
             monDataList.push(thing)
 
         }
-        // console.log(monDataList);
-        // console.log(yeardata);
-        // console.log(getMonData['oct']);
-
         res.status(200).send({
-            message: 'fetched sales data', productChart
-            , categotyList, monDataList
+            message: 'fetched sales data',resData
         })
     } catch (error) {
         res.status(500).send({ message: error.message })
--- src/route/Sales/Sales.js
+++ src/route/Sales/Sales.js
@@ -3,5 +3,5 @@ import salesController from '../../controller/Sales/Sales.js';
 import auth from '../../common/auth.js';
 const route=express.Router();
 route.post('/saleslistadd',auth.validate,salesController.salesDataAdd)
-route.get('/getsalelist/:email',auth.validate,salesController.getSalesData)
+route.get('/getsalelist/:email',salesController.getSalesData)
 export default route;
